42cf56c125847ba9fb4c224079b04439e7b024e4
Rafael <tibian24h@hotmail.com>
2026-03-19 21:58:27 -0300

    Corrige SSL do PostgreSQL para deploy no Render

--- src/config/db.js
+++ src/config/db.js
@@ -2,6 +2,9 @@ const { Pool } = require("pg");
 
 const env = require("./env");
 
-const pool = new Pool(env.db);
+const pool = new Pool({
+  ...env.db,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+});
 
 module.exports = pool;
